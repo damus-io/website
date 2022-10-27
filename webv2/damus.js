dfef728d9125548fb543e4da0f5870e6f8380470
William Casarin <jb55@jb55.com>
2022-10-27 14:11:43 -0700

    move webv2 to master
    no point having a branch for this.

---
+++ webv2/damus.js
@@ -0,0 +1,574 @@
+
+let DSTATE
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+function insert_event_sorted(evs, new_ev) {
+        for (let i = 0; i < evs.length; i++) {
+                const ev = evs[i]
+
+                if (new_ev.id === ev.id) {
+                        return false
+                }
+
+                if (new_ev.created_at > ev.created_at) {
+                        evs.splice(i, 0, new_ev)
+                        return true
+                }
+        }
+
+        evs.push(new_ev)
+        return true
+}
+
+function init_contacts() {
+	return {
+		event: null,
+		friends: new Set(),
+		friend_of_friends: new Set(),
+	}
+}
+
+function init_home_model() {
+	return {
+		done_init: false,
+		loading: true,
+		all_events: {},
+		events: [],
+		profiles: {},
+		last_event_of_kind: {},
+		contacts: init_contacts()
+	}
+}
+
+async function damus_web_init(thread)
+{
+	const {RelayPool} = nostrjs
+	const pool = RelayPool(["wss://relay.damus.io"])
+	const now = (new Date().getTime()) / 1000
+	const model = init_home_model()
+	DSTATE = model
+
+	const ids = {
+		comments: "comments",//uuidv4(),
+		profiles: "profiles",//uuidv4(),
+		account: "account",//uuidv4(),
+		home: "home",//uuidv4(),
+		contacts: "contacts",//uuidv4(),
+		notifications: "notifications",//uuidv4(),
+		dms: "dms",//uuidv4(),
+	}
+
+	model.pubkey = get_pubkey()
+	if (!model.pubkey)
+		return
+	model.pool = pool
+	model.el = document.querySelector("#posts")
+
+	pool.on('open', (relay) => {
+		//let authors = followers
+		// TODO: fetch contact list
+		log_debug("relay connected", relay.url)
+		
+		if (!model.done_init) {
+			model.loading = false
+			send_initial_filters(ids.account, model.pubkey, relay)
+		} else {
+			send_home_filters(ids, model, relay)
+		}
+		//relay.subscribe(comments_id, {kinds: [1,42], limit: 100})
+	});
+
+	pool.on('event', (relay, sub_id, ev) => {
+		handle_home_event(ids, model, relay, sub_id, ev)
+	})
+
+	pool.on('eose', async (relay, sub_id) => {
+		if (sub_id === ids.home) {
+			handle_comments_loaded(ids.profiles, model, relay)
+		} else if (sub_id === ids.profiles) {
+			handle_profiles_loaded(ids.profiles, model, relay)
+		}
+	})
+
+	return pool
+}
+
+let rerender_home_timer
+function handle_home_event(ids, model, relay, sub_id, ev) {
+	model.all_events[ev.id] = ev
+
+	switch (sub_id) {
+	case ids.home:
+		if (ev.content !== "")
+			insert_event_sorted(model.events, ev)
+		if (model.realtime) {
+			if (rerender_home_timer)
+				clearTimeout(rerender_home_timer)
+			rerender_home_timer = setTimeout(render_home_view.bind(null, model), 200)
+		}
+		break;
+	case ids.account:
+		switch (ev.kind) {
+		case 3:
+			model.loading = false
+			process_contact_event(model, ev)
+			model.done_init = true
+			model.pool.unsubscribe(ids.account, [relay])
+			break
+		case 0:
+			handle_profile_event(model, ev)
+			break
+		}
+	case ids.profiles:
+		try {
+			model.profiles[ev.pubkey] = JSON.parse(ev.content)
+		} catch {
+			console.log("failed to parse", ev.content)
+		}
+	}
+}
+
+function handle_profile_event(model, ev) {
+	console.log("PROFILE", ev)
+}
+
+function send_initial_filters(account_id, pubkey, relay) {
+	const filter = {authors: [pubkey], kinds: [3], limit: 1}
+	relay.subscribe(account_id, filter)
+}
+
+function send_home_filters(ids, model, relay) {
+	const friends = contacts_friend_list(model.contacts)
+	friends.push(model.pubkey)
+
+	const contacts_filter = {kinds: [0], authors: friends}
+	const dms_filter = {kinds: [4], limit: 500}
+	const our_dms_filter = {kinds: [4], authors: [ model.pubkey ], limit: 500}
+	const home_filter = {kinds: [1,42,6,7], authors: friends, limit: 500}
+	const notifications_filter = {kinds: [1,42,6,7], "#p": [model.pubkey], limit: 100}
+
+	let home_filters = [home_filter]
+	let notifications_filters = [notifications_filter]
+	let contacts_filters = [contacts_filter]
+	let dms_filters = [dms_filter, our_dms_filter]
+
+	let last_of_kind = {}
+	if (relay) {
+		last_of_kind =
+			model.last_event_of_kind[relay] =
+			model.last_event_of_kind[relay] || {}
+	}
+
+        update_filters_with_since(last_of_kind, home_filters)
+        update_filters_with_since(last_of_kind, contacts_filters)
+        update_filters_with_since(last_of_kind, notifications_filters)
+        update_filters_with_since(last_of_kind, dms_filters)
+
+	const subto = relay? [relay] : undefined
+	model.pool.subscribe(ids.home, home_filters, subto)
+	model.pool.subscribe(ids.contacts, contacts_filters, subto)
+	model.pool.subscribe(ids.notifications, notifications_filters, subto)
+	model.pool.subscribe(ids.dms, dms_filters, subto)
+}
+
+function get_since_time(last_event) {
+	if (!last_event) {
+		return null
+	}
+
+	return last_event.created_at - 60 * 10
+}
+
+function update_filter_with_since(last_of_kind, filter) {
+	const kinds = filter.kinds || []
+	let initial = null
+	let earliest = kinds.reduce((earliest, kind) => {
+		const last = last_of_kind[kind]
+		let since = get_since_time(last) 
+
+		if (!earliest) {
+			if (since === null)
+				return null
+
+			return since
+		}
+
+		if (since === null)
+			return earliest
+
+		return since < earliest ? since : earliest
+
+	}, initial)
+
+	if (earliest)
+		filter.since = earliest
+}
+
+function update_filters_with_since(last_of_kind, filters) {
+	for (const filter of filters) {
+		update_filter_with_since(last_of_kind, filter)
+	}
+}
+
+function contacts_friend_list(contacts) {
+	return Array.from(contacts.friends)
+}
+
+function process_contact_event(model, ev) {
+	load_our_contacts(model.contacts, model.pubkey, ev)
+	load_our_relays(model.pubkey, model.pool, ev)
+	add_contact_if_friend(model.contacts, ev)
+}
+
+function add_contact_if_friend(contacts, ev) {
+	if (!contact_is_friend(contacts, ev.pubkey)) {
+		return
+	}
+
+	add_friend_contact(contacts, ev)
+}
+
+function contact_is_friend(contacts, pk) {
+	return contacts.friends.has(pk)
+}
+
+function add_friend_contact(contacts, contact) {
+	contacts.friends[contact.pubkey] = true
+
+        for (const tag of contact.tags) {
+            if (tag.count >= 2 && tag[0] == "p") {
+                contacts.friend_of_friends.add(tag[1])
+            }
+        }
+}
+
+function load_our_relays(our_pubkey, pool, ev) {
+	if (ev.pubkey != our_pubkey)
+		return
+
+	let relays
+	try {
+		relays = JSON.parse(ev.content)
+	} catch (e) {
+		log_debug("error loading relays", e)
+		return
+	}
+
+	for (const relay of Object.keys(relays)) {
+		log_debug("adding relay", relay)
+		if (!pool.has(relay))
+			pool.add(relay)
+	}
+}
+
+function log_debug(fmt, ...args) {
+	console.log("[debug] " + fmt, ...args)
+}
+
+function load_our_contacts(contacts, our_pubkey, ev) {
+	if (ev.pubkey !== our_pubkey)
+		return
+
+	contacts.event = ev
+
+	for (const tag of ev.tags) {
+		if (tag.length > 1 && tag[0] === "p") {
+			contacts.friends.add(tag[1])
+		}
+	}
+}
+
+function handle_profiles_loaded(profiles_id, model, relay) {
+	// stop asking for profiles
+	model.pool.unsubscribe(profiles_id, relay)
+	model.realtime = true
+	render_home_view(model)
+}
+
+function debounce(f, interval) {
+	let timer = null;
+	let first = true;
+
+	return (...args) => {
+		clearTimeout(timer);
+		return new Promise((resolve) => {
+			timer = setTimeout(() => resolve(f(...args)), first? 0 : interval);
+			first = false
+		});
+	};
+}
+
+// load profiles after comment notes are loaded
+function handle_comments_loaded(profiles_id, model, relay)
+{
+	const pubkeys = model.events.reduce((s, ev) => {
+		s.add(ev.pubkey)
+		return s
+	}, new Set())
+	const authors = Array.from(pubkeys)
+
+	// load profiles
+	const filter = {kinds: [0], authors: authors}
+	console.log("subscribe", profiles_id, filter, relay)
+	model.pool.subscribe(profiles_id, filter, relay)
+}
+
+function render_home_view(model) {
+	log_debug("rendering home view")
+	model.el.innerHTML = render_events(model)
+}
+
+function render_events(model) {
+	const render = render_event.bind(null, model)
+	return model.events.map(render).join("\n")
+}
+
+function render_event(model, ev, opts={}) {
+	const profile = model.profiles[ev.pubkey] || {
+		name: "anon",
+		display_name: "Anonymous",
+	}
+	const delta = time_delta(new Date().getTime(), ev.created_at*1000)
+	const pk = ev.pubkey
+	const bar = opts.nobar? "" : render_action_bar(ev) 
+	return `
+	<div class="comment">
+		<div class="info">
+			${render_name(ev.pubkey, profile)}
+			<span>${delta}</span>
+		</div>
+		<img class="pfp" onerror="this.onerror=null;this.src='${robohash(pk)}';" src="${get_picture(pk, profile)}">
+		<p>
+		${format_content(ev.content)}
+
+		${bar}
+		</p>
+	</div>
+	`
+}
+
+function close_reply() {
+	const modal = document.querySelector("#reply-modal")
+	modal.style.display = "none";
+}
+
+function gather_reply_tags(pubkey, from) {
+	let tags = []
+	for (const tag of from.tags) {
+		if (tag.length >= 2) {
+			if (tag[0] === "e") {
+				tags.push(tag)
+			} else if (tag[0] === "p" && tag[1] !== pubkey) {
+				tags.push(tag)
+			}
+		}
+	}
+	tags.push(["e", from.id, "", "reply"])
+	if (from.pubkey !== pubkey)
+		tags.push(["p", from.pubkey])
+	return tags
+}
+
+async function create_reply(privkey, pubkey, content, from) {
+	const tags = gather_reply_tags(pubkey, from)
+	const created_at = Math.floor(new Date().getTime() / 1000)
+	const kind = from.kind
+
+	let reply = { pubkey, tags, content, created_at, kind }
+
+	reply.id = await nostrjs.calculate_id(reply)
+	reply.sig = await sign_id(privkey, reply.id)
+
+	return reply
+}
+
+async function send_reply() {
+	const modal = document.querySelector("#reply-modal")
+	const replying_to = modal.querySelector("#replying-to")
+	const evid = replying_to.dataset.evid
+	const ev = DSTATE.all_events[evid]
+
+	const { pool } = DSTATE
+	const content = document.querySelector("#reply-content").value
+	const pubkey = get_pubkey()
+	const privkey = get_privkey()
+
+	let reply = await create_reply(privkey, pubkey, content, ev)
+	console.log(nostrjs.event_commitment(reply), reply)
+	pool.send(["EVENT", reply])
+
+	close_reply()
+}
+
+function bech32_decode(pubkey) {
+	const decoded = bech32.decode(pubkey)
+	const bytes = fromWords(decoded.words)
+	return nostrjs.hex_encode(bytes)
+}
+
+function get_local_state(key) {
+	if (DSTATE[key] != null)
+		return DSTATE[key]
+
+	return localStorage.getItem(key)
+}
+
+function set_local_state(key, val) {
+	DSTATE[key] = val
+	localStorage.setItem(key, val)
+}
+
+function get_pubkey() {
+	let pubkey = get_local_state('pubkey')
+
+	if (pubkey)
+		return pubkey
+
+	pubkey = prompt("Enter pubkey (hex or npub)")
+
+	if (!pubkey)
+		throw new Error("Need pubkey to continue")
+
+	if (pubkey[0] === "n")
+		pubkey = bech32_decode(pubkey)
+
+	set_local_state('pubkey', pubkey)
+	return pubkey
+}
+
+function get_privkey() {
+	let privkey = get_local_state('privkey')
+
+	if (privkey)
+		return privkey
+
+	if (!privkey)
+		privkey = prompt("Enter private key")
+
+	if (!privkey)
+		throw new Error("can't get privkey")
+
+	if (privkey[0] === "n") {
+		privkey = bech32_decode(privkey)
+	}
+
+	set_local_state('privkey', privkey)
+
+	return privkey
+}
+
+async function sign_id(privkey, id)
+{
+	//const digest = nostrjs.hex_decode(id)
+	const sig = await nobleSecp256k1.schnorr.sign(id, privkey)
+	return nostrjs.hex_encode(sig)
+}
+
+function reply_to(evid) {
+	const modal = document.querySelector("#reply-modal")
+	const replying = modal.style.display === "none";
+	const replying_to = modal.querySelector("#replying-to")
+
+	replying_to.dataset.evid = evid
+	const ev = DSTATE.all_events[evid]
+	replying_to.innerHTML = render_event(DSTATE, ev, {nobar: true})
+
+	modal.style.display = replying? "block" : "none";
+}
+
+function render_action_bar(ev) {
+	return `
+	<a href="javascript:reply_to('${ev.id}')">reply</a>
+	`
+}
+
+function convert_quote_blocks(content)
+{
+	const split = content.split("\n")
+	let blockin = false
+	return split.reduce((str, line) => {
+		if (line !== "" && line[0] === '>') {
+			if (!blockin) {
+				str += "<span class='quote'>"
+				blockin = true
+			}
+			str += sanitize(line.slice(1))
+		} else {
+			if (blockin) {
+				blockin = false
+				str += "</span>"
+			}
+			str += sanitize(line)
+		}
+		return str + "<br/>"
+	}, "")
+}
+
+function format_content(content)
+{
+	return convert_quote_blocks(content)
+}
+
+function sanitize(content)
+{
+	if (!content)
+		return ""
+	return content.replaceAll("<","&lt;").replaceAll(">","&gt;")
+}
+
+function robohash(pk) {
+	return "https://robohash.org/" + pk
+}
+
+function get_picture(pk, profile)
+{
+	return sanitize(profile.picture) || robohash(pk)
+}
+
+function render_name(pk, profile={})
+{
+	const display_name = profile.display_name || profile.user
+	const username = profile.name || "anon"
+	const name = display_name || username
+
+	return `<div class="username">${sanitize(name)}</div>`
+}
+
+function time_delta(current, previous) {
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}
---
+++ webv2/nostr.js
@@ -0,0 +1,360 @@
+const nostrjs = (function nostrlib() {
+const WS = typeof WebSocket !== 'undefined' ? WebSocket : require('ws')
+
+function RelayPool(relays, opts)
+{
+	if (!(this instanceof RelayPool))
+		return new RelayPool(relays)
+
+	this.onfn = {}
+	this.relays = []
+
+	for (const relay of relays) {
+		this.add(relay)
+	}
+
+	return this
+}
+
+RelayPool.prototype.close = function relayPoolClose() {
+	for (const relay of this.relays) {
+		relay.close()
+	}
+}
+
+RelayPool.prototype.on = function relayPoolOn(method, fn) {
+	for (const relay of this.relays) {
+		this.onfn[method] = fn
+		relay.onfn[method] = fn.bind(null, relay)
+	}
+}
+
+RelayPool.prototype.has = function relayPoolHas(relayUrl) {
+	for (const relay of this.relays) {
+		if (relay.url === relayUrl)
+			return true
+	}
+
+	return false
+}
+
+RelayPool.prototype.setupHandlers = function relayPoolSetupHandlers()
+{
+	// setup its message handlers with the ones we have already
+	const keys = Object.keys(this.onfn)
+	for (const handler of keys) {
+		for (const relay of this.relays) {
+			relay.onfn[handler] = this.onfn[handler].bind(null, relay)
+		}
+	}
+}
+
+RelayPool.prototype.remove = function relayPoolRemove(url) {
+	let i = 0
+
+	for (const relay of this.relays) {
+		if (relay.url === url) {
+			relay.ws && relay.ws.close()
+			this.relays = this.replays.splice(i, 1)
+			return true
+		}
+
+		i += 1
+	}
+
+	return false
+}
+
+RelayPool.prototype.subscribe = function relayPoolSubscribe(sub_id, filters, relay_ids) {
+	const relays = relay_ids ? this.find_relays(relay_ids) : this.relays
+	for (const relay of relays) {
+		relay.subscribe(sub_id, filters)
+	}
+}
+
+RelayPool.prototype.unsubscribe = function relayPoolUnsubscibe(sub_id, relay_ids) {
+	const relays = relay_ids ? this.find_relays(relay_ids) : this.relays
+	for (const relay of relays) {
+		relay.unsubscribe(sub_id)
+	}
+}
+
+RelayPool.prototype.send = function relayPoolSend(payload, relay_ids) {
+	const relays = relay_ids ? this.find_relays(relay_ids) : this.relays
+	for (const relay of relays) {
+		relay.send(payload)
+	}
+}
+
+RelayPool.prototype.add = function relayPoolAdd(relay) {
+	if (relay instanceof Relay) {
+		if (this.has(relay.url))
+			return false
+
+		this.relays.push(relay)
+		this.setupHandlers()
+		return true
+	}
+
+	if (this.has(relay))
+		return false
+
+	const r = Relay(relay, this.opts)
+	this.relays.push(r)
+	this.setupHandlers()
+	return true
+}
+
+RelayPool.prototype.find_relays = function relayPoolFindRelays(relay_ids) {
+	if (relay_ids instanceof Relay)
+		return [relay_ids]
+
+	if (relay_ids.length === 0)
+		return []
+
+	if (!relay_ids[0])
+		throw new Error("what!?")
+
+	if (relay_ids[0] instanceof Relay)
+		return relay_ids
+
+	return this.relays.reduce((acc, relay) => {
+		if (relay_ids.some((rid) => relay.url === rid))
+			acc.push(relay)
+		return acc
+	}, [])
+}
+
+Relay.prototype.wait_connected = async function relay_wait_connected(data) {
+	let retry = 1000
+	while (true) {
+		if (this.ws.readyState !== 1) {
+			await sleep(retry)
+			retry *= 1.5
+		}
+		else {
+			return
+		}
+	}
+}
+
+
+function Relay(relay, opts={})
+{
+	if (!(this instanceof Relay))
+		return new Relay(relay, opts)
+
+	this.url = relay
+	this.opts = opts
+
+	if (opts.reconnect == null)
+		opts.reconnect = true
+
+	const me = this
+	me.onfn = {}
+
+	init_websocket(me)
+
+	return this
+}
+
+function init_websocket(me) {
+	let ws
+	try {
+		ws = me.ws = new WS(me.url);
+	} catch(e) {
+		return null
+	}
+	return new Promise((resolve, reject) => {
+		let resolved = false
+		ws.onmessage = (m) => { handle_nostr_message(me, m) }
+		ws.onclose = () => { 
+			if (me.onfn.close) 
+				me.onfn.close() 
+			if (me.reconnecting)
+				return reject(new Error("close during reconnect"))
+			if (!me.manualClose && me.opts.reconnect)
+				reconnect(me)
+		}
+		ws.onerror = () => { 
+			if (me.onfn.error)
+				me.onfn.error() 
+			if (me.reconnecting)
+				return reject(new Error("error during reconnect"))
+			if (me.opts.reconnect)
+				reconnect(me)
+		}
+		ws.onopen = () => {
+			if (me.onfn.open)
+				me.onfn.open()
+
+			if (resolved) return
+
+			resolved = true
+			resolve(me)
+		}
+	});
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function reconnect(me)
+{
+	const reconnecting = true
+	let n = 100
+	try {
+		me.reconnecting = true
+		await init_websocket(me)
+		me.reconnecting = false
+	} catch {
+		//console.error(`error thrown during reconnect... trying again in ${n} ms`)
+		await sleep(n)
+		n *= 1.5
+	}
+}
+
+Relay.prototype.on = function relayOn(method, fn) {
+	this.onfn[method] = fn
+}
+
+Relay.prototype.close = function relayClose() {
+	if (this.ws) {
+		this.manualClose = true
+		this.ws.close()
+	}
+}
+
+Relay.prototype.subscribe = function relay_subscribe(sub_id, filters) {
+	if (Array.isArray(filters))
+		this.send(["REQ", sub_id, ...filters])
+	else
+		this.send(["REQ", sub_id, filters])
+}
+
+Relay.prototype.unsubscribe = function relay_unsubscribe(sub_id) {
+	this.send(["CLOSE", sub_id])
+}
+
+Relay.prototype.send = async function relay_send(data) {
+	await this.wait_connected()
+	this.ws.send(JSON.stringify(data))
+}
+
+function handle_nostr_message(relay, msg)
+{
+	let data
+	try {
+		data = JSON.parse(msg.data)
+	} catch (e) {
+		console.error("handle_nostr_message", e)
+		return
+	}
+	if (data.length >= 2) {
+		switch (data[0]) {
+		case "EVENT":
+			if (data.length < 3)
+				return
+			return relay.onfn.event && relay.onfn.event(data[1], data[2])
+		case "EOSE":
+			return relay.onfn.eose && relay.onfn.eose(data[1])
+		case "NOTICE":
+			return relay.onfn.notice && relay.onfn.notice(...data.slice(1))
+		}
+	}
+}
+
+async function sha256(message) {
+	if (crypto.subtle) {
+		const buffer = await crypto.subtle.digest('SHA-256', message);
+		return new Uint8Array(buffer);
+	} else if (require) {
+		const { createHash } = require('crypto');
+		const hash = createHash('sha256');
+		[message].forEach((m) => hash.update(m));
+		return Uint8Array.from(hash.digest());
+	} else {
+		throw new Error("The environment doesn't have sha256 function");
+	}
+}
+
+async function calculate_id(ev) {
+	const commit = event_commitment(ev)
+	const buf = new TextEncoder().encode(commit);                    
+	return hex_encode(await sha256(buf))
+}
+
+function event_commitment(ev) {
+	const {pubkey,created_at,kind,tags,content} = ev
+	return JSON.stringify([0, pubkey, created_at, kind, tags, content])
+}
+
+function hex_char(val) {
+	if (val < 10)
+		return String.fromCharCode(48 + val)
+	if (val < 16)
+		return String.fromCharCode(97 + val - 10)
+}
+
+function hex_encode(buf) {
+	let str = ""
+	for (let i = 0; i < buf.length; i++) {
+		const c = buf[i]
+		str += hex_char(c >> 4)
+		str += hex_char(c & 0xF)
+	}
+	return str
+}
+
+function char_to_hex(cstr) {
+	const c = cstr.charCodeAt(0)
+	// c >= 0 && c <= 9
+	if (c >= 48 && c <= 57) {
+		return c - 48;
+	}
+	// c >= a && c <= f
+ 	if (c >= 97 && c <= 102) {
+		return c - 97 + 10;
+	}
+	// c >= A && c <= F
+ 	if (c >= 65 && c <= 70) {
+		return c - 65 + 10;
+	}
+	return -1;
+}
+
+
+function hex_decode(str, buflen)
+{
+	let bufsize = buflen || 33
+	let c1, c2
+	let i = 0
+	let j = 0
+	let buf = new Uint8Array(bufsize)
+	let slen = str.length
+	while (slen > 1) {
+		if (-1==(c1 = char_to_hex(str[j])) || -1==(c2 = char_to_hex(str[j+1])))
+			return null;
+		if (!bufsize)
+			return null;
+		j += 2
+		slen -= 2
+		buf[i++] = (c1 << 4) | c2
+		bufsize--;
+	}
+
+	return buf
+}
+
+return {
+	RelayPool,
+	calculate_id,
+	event_commitment,
+	hex_encode,
+	hex_decode,
+}
+})()
+
+if (typeof module !== 'undefined' && module.exports)
+	module.exports = nostrjs
