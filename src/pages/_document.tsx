975cd1a537b88b135165e4c96c10f163fe6fb466
Daniel D’Aquino <daniel@daquino.me>
2023-10-23 22:29:22 -0700

    Add meta tags from old website version

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="Damus"/>
+        <meta property="og:description" content="A new social network that you control"/>
+        <meta property="og:image" content="https://damus.io/logo_icon.png"/>
+        <meta property="og:url" content="https://damus.io"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:image" content="https://damus.io/logo_icon.png"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
