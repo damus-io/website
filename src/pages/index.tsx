6d4962329701df209432263d3494f34ed8911cdc
Daniel D’Aquino <daniel@daquino.me>
2023-10-25 13:25:28 -0700

    Implement BannedInChina section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { Hero } from '@/components/sections/Hero'
+import Head from "next/head";
+import { useIntl } from 'react-intl'
+import { BannedInChina } from '@/components/sections/BannedInChina'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
+  const intl = useIntl()
+
+  return (<>
+    <Head>
+      <title>Damus</title>
+      <meta name="description" content={ intl.formatMessage({ id: "home.meta_description", defaultMessage: "Damus is a new social network that you control. Available now on iOS, iPad and MacOS (M1/M2)" }) }/>
+    </Head>
     <main>
       <Hero/>
+      <BannedInChina/>
     </main>
-  )
+  </>)
 }
