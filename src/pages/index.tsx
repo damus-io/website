2c18c0d0d951440e3a6c4869c10a3710efd78cf4
Daniel D’Aquino <daniel@daquino.me>
2023-10-31 04:53:27 +0000

    Add language detection logic to main page
    Signed-off-by: Daniel D’Aquino <daniel@daquino.me>
    Signed-off-by: William Casarin <jb55@jb55.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,14 +15,38 @@ import { Contribute } from '@/components/sections/Contribute';
 import { FinalCTA } from '@/components/sections/FinalCTA';
 import { Benefits } from '@/components/sections/Benefits';
 import { Home } from '@/components/pages/home';
+import { useEffect, useMemo } from 'react';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function HomePage() {
+  // Automatically detect the user's locale based on their browser settings
+  const [language, setLanguage] = useState("en");
+  const [messages, setMessages] = useState(English);
+
+  useEffect(() => {
+    setLanguage(navigator.language);
+  }, []);
+
+  useEffect(() => {
+    switch (language) {
+      case "en":
+        setMessages(English);
+        break;
+      case "ja":
+        setMessages(Japanese);
+        break;
+      default:
+        setMessages(English);
+        break;
+    }
+  }, [language]);
+
   return (<>
     <IntlProvider
-            locale={"en"}
-            messages={English}
+            locale={language}
+            messages={messages}
             onError={() => null}>
       <Home/>
     </IntlProvider>
