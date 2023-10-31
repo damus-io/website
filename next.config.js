69f63d5e9a7abfebfc04790e41eae9a604d486b3
Daniel D’Aquino <daniel@daquino.me>
2023-10-31 04:53:07 +0000

    Change build target to static export
    Fixes: https://github.com/damus-io/website/issues/15
    Signed-off-by: Daniel D’Aquino <daniel@daquino.me>
    Signed-off-by: William Casarin <jb55@jb55.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  i18n: {
-    locales: ['en-US', 'ja-JP'],
-    defaultLocale: 'en-US',
+  images: {
+    unoptimized: true,
   },
 }
 
