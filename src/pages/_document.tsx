b22e06fcb9ea9e3bf06b019ee8e95b4646f75f9b
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2023-12-11 14:34:50 -0800

    Update _document.tsx
    SEO improvements: keywords, twitter description and title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,11 @@ export default function Document() {
         <meta property="og:description" content="A new social network that you control"/>
         <meta property="og:image" content="https://damus.io/logo_icon.png"/>
         <meta property="og:url" content="https://damus.io"/>
+        <meta name="keywords" content="Damus, nostr client, decentralized, social media, censorship resistant, open source, iOS, iPad, macOS">
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:image" content="https://damus.io/logo_icon.png"/>
+        <meta name="twitter:description" content="Damus - A new social network that you control">
+        <meta name="twitter:title" content="Damus">
       </Head>
       <body>
         <Main />
