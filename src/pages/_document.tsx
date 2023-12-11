damus-io/website

commit 24bb687f71c318cbc92df898e8403ba36f279922

Update _document.tsx
SEO improvements: keywords, twitter description and title

Closes: https://github.com/damus-io/website/pull/17
Reviewed-by: William Casarin <jb55@jb55.com>
Signed-off-by: William Casarin <jb55@jb55.com>

src/pages/_document.tsx
@@ -8,8 +8,11 @@ export default function Document() {
         <meta property="og:description" content="A new social network that you control"/>
         <meta property="og:image" content="https://damus.io/logo_icon.png"/>
         <meta property="og:url" content="https://damus.io"/>
+        <meta name="keywords" content="Damus, nostr client, decentralized, social media, censorship resistant, open source, iOS, iPad, macOS">
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:image" content="https://damus.io/logo_icon.png"/>
+        <meta name="twitter:description" content="Damus - A new social network that you control">
+        <meta name="twitter:title" content="Damus">
       </Head>
       <body>
         <Main />

commit ae285a22f40600aa8642fd9c5e9acd69576dcdcc

meta: fix broken elements
Fixes: 24bb687f71c3 ("Update _document.tsx")

src/pages/_document.tsx
@@ -8,11 +8,11 @@ export default function Document() {
         <meta property="og:description" content="A new social network that you control"/>
         <meta property="og:image" content="https://damus.io/logo_icon.png"/>
         <meta property="og:url" content="https://damus.io"/>
-        <meta name="keywords" content="Damus, nostr client, decentralized, social media, censorship resistant, open source, iOS, iPad, macOS">
+        <meta name="keywords" content="Damus, nostr client, decentralized, social media, censorship resistant, open source, iOS, iPad, macOS"/>
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:image" content="https://damus.io/logo_icon.png"/>
-        <meta name="twitter:description" content="Damus - A new social network that you control">
-        <meta name="twitter:title" content="Damus">
+        <meta name="twitter:description" content="Damus - A new social network that you control"/>
+        <meta name="twitter:title" content="Damus"/>
       </Head>
       <body>
         <Main />