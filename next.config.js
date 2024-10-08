1b6209afc40c463a04d7267a155e840736301c39
Daniel D’Aquino <daniel@daquino.me>
2024-10-07 17:08:56 -0700

    Improve build process
    This commit improves the website build process by using `trailingSlash`
    in `next.config.js` instead of manually adding symbolic links during the
    build script.
    
    It also adds the option to create a development build with
    user-configurable environment via an `.env` file
    
    Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  // Build `/src/pages/example/index.tsx` to `out/example/index.html` instead of `out/example.html`
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
