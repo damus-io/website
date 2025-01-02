e8d67ffc96888f4be32f78224f61a54addc2be48
Daniel D’Aquino <daniel@daquino.me>
2025-01-02 14:06:57 +0900

    Add Nostr note component
    This adds a nostr note component that can be rendered from a notecrumbs
    parsed note
    
    Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

--- src/components/note/NostrNoteView.tsx
+++ src/components/note/NostrNoteView.tsx
@@ -1,3 +1,7 @@
+import { cn } from "@/lib/utils";
+import { useMemo } from "react";
+import Image from "next/image";
+
 export interface ParsedNote {
   note: Note
   parsed_content: ParsedContentBlock[];
@@ -44,12 +48,77 @@ export interface Note {
 export interface NostrNoteViewProps {
   note: ParsedNote;
   className?: string;
+  style?: React.CSSProperties;
 }
 
 export function NostrNoteView(props: NostrNoteViewProps) {
+  const profileContent = useMemo(() => {  // JSON parsing is expensive, so we memoize it
+    return JSON.parse(props.note.profile.content) as ProfileContent;
+  }, [props.note.profile.content]);
+  const timestamp = new Date(props.note.note.created_at * 1000).toLocaleDateString();
+  const displayName = profileContent.displayName || profileContent.name;
+
   return (
-    <div className="shadow-xl text-red-500">
-      {props.note.note.content}
+    <div className={cn("p-6 rounded-3xl shadow-lg border border-black/20", props.className)} style={props.style}>
+      <div className="flex flex-col gap-y-3">
+        <div className="flex items-center gap-x-3 text-xl">
+          <Image
+            src={profileContent.picture}
+            className="w-12 h-12 rounded-full"
+            width={48}
+            height={48}
+            alt={displayName}
+          />
+          <div className="flex flex-col">
+            <div className="font-bold text-2xl">
+              {displayName}
+            </div>
+            <div className="text-gray-400 text-sm">
+              {timestamp}
+            </div>
+          </div>
+        </div>
+        <div className="text-gray-700 whitespace-pre-wrap break-words">
+          {props.note.parsed_content.map((block, i) => {
+            return <NoteBlock key={i} block={block} />
+          })}
+          {/* {props.note.note.content} */}
+        </div>
+      </div>
     </div>
   )
+}
+
+export function NoteBlock({ block }: { block: ParsedContentBlock }): JSX.Element | null {
+  if (block.text) {
+    return <span>{block.text}</span>;
+  } else if (block.mention) {
+    return <span className="text-damuspink-600 hover:underline"><a target="_blank" href={mentionLinkAddress(block.mention)}>@{shortenMention(block.mention)}</a></span>;
+  } else if (block.url) {
+    return (
+      /\.(jpg|jpeg|png|gif)$/.test(block.url) ?
+        <div className="my-2 flex items-center justify-center w-full h-auto max-h-96 overflow-hidden">
+          <img src={block.url} className="w-full"/>
+        </div>
+        :
+        <a href={block.url} target="_blank" rel="noopener noreferrer">{block.url}</a>
+    );
+  } else if (block.hashtag) {
+    return <span className="text-damuspink-600 hover:underline"><a href={"damus:t:" + block.hashtag}>#{block.hashtag}</a></span>;
+  } else {
+    return null;
+  }
+}
+
+function mentionLinkAddress(mention: string) {
+  if (mention.startsWith("note")) {
+    return "https://damus.io/" + mention;
+  }
+  else {
+    return "https://njump.me/" + mention;
+  }
+}
+
+function shortenMention(npub: string) {
+  return npub.substring(0, 8) + ":" + npub.substring(npub.length - 8);
 }
